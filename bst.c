984aa5831c8f2bd0f0a5085c1d885a19a0d324e4
DominikStefanowicz <dominik.stefanowicz@gmail.com>
2015-06-14 17:06:38 +0200

    Integrated possibility of adding a new node into tree-structure.

--- bst.c
+++ bst.c
@@ -15,17 +15,46 @@ struct Node *Create_Element(int value)
 
 int Compare_Keys(struct Node *element, int id_value)
 {
-    if ((element->left_branch != NULL) && (element->key_value < id_value))
-        return 1;
-    else if ((element->left_branch != NULL) && (element->key_value > id_value))
-        return -1;
-    else return 0;
+    if (element->key_value == id_value) return 0;
+    else if (element->key_value < id_value)return -1;
+    else return 1;
+}
+
+struct Node *Return_Node(struct Node **root, int val, int key_val){
+
+    if (val < 0){
+        if ((*root)->left_branch == NULL){   // enter new node
+            (*root)->left_branch = Create_Element(key_val);
+            return 0;
+        }
+        else return (*root)->left_branch;
+    }else{
+        if ((*root)->right_branch == NULL){   // enter new node
+            (*root)->right_branch = Create_Element(key_val);
+            return 0;
+        }
+        else return (*root)->right_branch;
+    }
 }
 
 void Insert_Element(struct Node **root, int id_value)
 {
     int a = 0;
-
+    int state = 0;
+    struct Node *tmp = NULL;
+    printf("BEGIN ADR %p %p\n", (void *)&root, root);
+    if ((*root) == NULL){
+        (*root) = Create_Element(id_value);
+        printf("First Element\n");
+        printf("BEGIN ADR %p %p\n", (void *)&root, root);
+    }else if (*root != NULL){
+        printf("Second Element\n");
+        state = Compare_Keys(&(*root), id_value);
+        tmp = Return_Node(&(*root), state, id_value);
+    }else{
+        (*root) = Create_Element(id_value);
+    }
+    /*
     if (*root != NULL){
         printf("ROOT : %d, ID : %d\n", (*root)->key_value, id_value);
         a = Compare_Keys((*root), id_value);
@@ -38,4 +67,5 @@ void Insert_Element(struct Node **root, int id_value)
     }else{
         (*root) = Create_Element(id_value);
     }
+    */
 }
--- main.c
+++ main.c
@@ -4,10 +4,14 @@
 int main()
 {
     struct Node *root = NULL;
+    printf("BEGIN ADR %p %p\n", (void *)&root, root);
     Insert_Element(&root, 6);
+    printf("BEGIN ADR %p %p\n", (void *)&root, root);
+    Insert_Element(&root, 5);
+/*    Insert_Element(&root, 7);
     Insert_Element(&root, 6);
     Insert_Element(&root, 5);
     Insert_Element(&root, 7);
-
+*/
     return 0;
 }
